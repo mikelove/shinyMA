2956930e681b64e391ad6e6026b1563aa962d85f
aoles <andrzej.oles@gmail.com>
2015-06-03 16:07:47 +0200

    Fixed DESeq2_plotMA_plotCounts example

--- DESeq2_plotMA_plotCounts/analysis.R
+++ DESeq2_plotMA_plotCounts/analysis.R
@@ -20,21 +20,10 @@ res <- results(dds)
 
 # this object will be used to locate points from click events.
 # take the log of x, so that points are 'close' in the log x axis
-data <- with(res, cbind(log10(baseMean), log2FoldChange))
+data <- with(res, cbind(baseMean, log2FoldChange))
 
 # we set the ylim so need to use
 ymax <- 2.5
 data[,2] <- pmin(ymax, pmax(-ymax, data[,2]))
 scale <- c(diff(range(data[,1])), 2*ymax)
 t.data.scaled <- t(data)/scale
-
-findClosest <- function(input) {
-  if (is.null(input$plotma_click)) {
-    1
-  } else {
-    # again log of x, so that points are 'close' in the log x axis
-    pt <- c(log10(input$plotma_click$x), y=input$plotma_click$y)
-    sqdists <- colMeans( ( t.data.scaled - pt/scale )^2 )
-    which.min(sqdists)
-  }
-}
