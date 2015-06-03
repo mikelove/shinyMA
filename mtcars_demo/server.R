2780731a53d18bafeb740a49603d5dd10fbbc97e
aoles <andrzej.oles@gmail.com>
2015-06-03 14:10:38 +0200

    Fixed mtcars_demo

--- mtcars_demo/server.R
+++ mtcars_demo/server.R
@@ -1,17 +1,28 @@
-source("analysis.R") # load data and define findClosest function
+data <- as.matrix(mtcars[,c(1,3)])
+scale <- c(diff(range(data[,1])), diff(range(data[,2])))
+t.data.scaled <- t(data)/scale
 
 library(shiny)
 shinyServer(function(input, output) {
-  output$myplot1 <- renderPlot({
-    idx <- findClosest(input)
+  idx = NULL
+  xy = reactive(c(input$plot_click$x, input$plot_click$y))
+  observe({
+    if (!is.null(xy())) {
+      sqdists <- colMeans( (t.data.scaled - xy()/scale )^2 ) 
+      idx <<- which.min(sqdists)  
+    }
+  })
+  
+  output$plot1 <- renderPlot({
+    xy()
     par(mar=c(5,5,3,2),cex.lab=2,cex.main=2,cex.axis=1.5)
     plot(mtcars[,c(1,3)])
-    points(mtcars[idx,c(1,3)],col="dodgerblue",cex=3,lwd=3)
+    if (!is.null(idx)) points(mtcars[idx,c(1,3)],col="dodgerblue",cex=3,lwd=3)
   })
-  output$myplot2 <- renderPlot({
-    idx <- findClosest(input)
+  output$plot2 <- renderPlot({
+    xy()
     par(mar=c(5,5,3,2),cex.lab=2,cex.main=2,cex.axis=1.5)
     plot(mtcars[,c(1,5)])
-    points(mtcars[idx,c(1,5)],col="dodgerblue",cex=3,lwd=3)
+    if (!is.null(idx)) points(mtcars[idx,c(1,5)],col="dodgerblue",cex=3,lwd=3)
   })
 })
